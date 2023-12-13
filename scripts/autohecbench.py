8fcbe719e5e4be68a11aec0390092ce5b7fc89cb
FRYoussef <f.r.youssef@hotmail.com>
2023-12-13 12:44:02 +0100

    Update default value for --warmup flag

--- scripts/autohecbench.py
+++ scripts/autohecbench.py
@@ -116,7 +116,7 @@ def main():
                         help='Output file for csv results')
     parser.add_argument('--repeat', '-r', type=int, default=1,
                         help='Repeat benchmark run')
-    parser.add_argument('--warmup', '-w', type=bool, default=True,
+    parser.add_argument('--warmup', '-w', type=bool, default=False,
                         help='Run a warmup iteration')
     parser.add_argument('--sycl-type', '-s', choices=['cuda', 'hip', 'opencl'], default='cuda',
                         help='Type of SYCL device to use')
